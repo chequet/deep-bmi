a2744b4cc99448c0ae16ca78f27446335f8ab14a
chloehequet <chloe.hequet@gmail.com>
2022-04-20 13:26:14 +0100

    network in class format

---
+++ EmbedNetwork.py
@@ -0,0 +1,34 @@
+import os
+import torch
+from torch import nn
+from EffectEmbeddingDataset import *
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+print(f"Using {device} device")
+
+class EmbedNetwork(nn.Module):
+    def __init__(self, n_inputs, reduction_factor, dropout):
+        super(EmbedNetwork, self).__init__()
+        n_features = n_inputs * 2
+        n_out1 = math.ceil(n_features / reduction_factor)
+        # (reduction_factor**2))
+        n_out2 = math.ceil(n_out1 / reduction_factor)
+        n_out3 = math.ceil(n_out2 / reduction_factor)
+        #self.flatten = nn.Flatten()
+        self.model = nn.Sequential(
+            torch.nn.Linear(n_features, n_out1),
+            torch.nn.Dropout(dropout),
+            torch.nn.LeakyReLU(),
+            torch.nn.Linear(n_out1, n_out2),
+            torch.nn.Dropout(dropout),
+            torch.nn.LeakyReLU(),
+            torch.nn.Linear(n_out2, n_out3),
+            torch.nn.Dropout(dropout),
+            torch.nn.LeakyReLU(),
+            torch.nn.Linear(n_out3, 1)
+        )
+
+    def forward(self, x):
+        #x = self.flatten(x)
+        logits = self.model(x)
+        return logits
