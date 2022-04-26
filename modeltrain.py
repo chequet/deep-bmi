e9c32b36ab316c90f0889a0a7e4802e9af4f4932
chloehequet <chloe.hequet@gmail.com>
2022-04-26 14:21:13 +0100

    sigmoid activation

--- modeltrain.py
+++ modeltrain.py
@@ -26,13 +26,13 @@ def sve_model(n_inputs, reduction_factor, dropout):
     model = torch.nn.Sequential(
         torch.nn.Linear(n_features, n_out1),
         torch.nn.Dropout(dropout),
-        torch.nn.LeakyReLU(),
+        torch.nn.Sigmoid(),
         torch.nn.Linear(n_out1, n_out2),
         torch.nn.Dropout(dropout),
-        torch.nn.LeakyReLU(),
+        torch.nn.Sigmoid(),
         torch.nn.Linear(n_out2, n_out3),
         torch.nn.Dropout(dropout),
-        torch.nn.LeakyReLU(),
+        torch.nn.Sigmoid(),
         torch.nn.Linear(n_out3, 1)
     )
     return model
