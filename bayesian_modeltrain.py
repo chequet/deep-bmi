a447bd882348cd51cd1fa0631672231bbbae883d
chloehequet <chloe.hequet@gmail.com>
2022-07-29 14:11:35 +0100

    debug

--- bayesian_modeltrain.py
+++ bayesian_modeltrain.py
@@ -49,8 +49,8 @@ def evaluate_regression(model, valid_iterator, samples,loss_fn, std_multiplier =
         batch = next(valid_iterator)
         X = batch[0].to(device)
         y = batch[1].to(device)
-        preds.append(model(X.float()).cpu())
-        gt.append(y.cpu())
+        preds.append(model(X.float()).detach().numpy())
+        gt.append(y.detach().numpy())
         i += 1
     preds = np.concatenate(preds)
     gt = np.concatenate(gt)
