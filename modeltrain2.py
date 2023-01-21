e2ce2610016e0f07ee24baa92056bc66b880b434
chloehequet <chloe.hequet@gmail.com>
2023-01-21 15:18:43 +0000

    0.4 dropout

--- modeltrain2.py
+++ modeltrain2.py
@@ -24,7 +24,7 @@ N_EPOCHS = int(sys.argv[4])
 ENCODING = int(sys.argv[3])
 BATCH_SIZE = 4096
 REDUCTIONS = [2,2,2]
-PATH = str(N_SNPS) + '_MSE_adam_leakyrelu_dropout05_' + str(ENCODING)
+PATH = str(N_SNPS) + '_MSE_adam_leakyrelu_dropout04_' + str(ENCODING)
 #==============================================
 
 def k_fold_split(train_dir, n=5):
@@ -120,7 +120,7 @@ def train_and_validate(arch, data_directory, train_set, val_set):
     print(device)
     # new model
     # -------------PARAMS-----------------------------------------------
-    model = FlexibleNet(arch, 0.5, 'LeakyReLU').to(device)
+    model = FlexibleNet(arch, 0.4, 'LeakyReLU').to(device)
     learning_rate = 0.0001
     #loss_fn = nn.HuberLoss()
     loss_fn = nn.MSELoss(reduction='mean')
