eeb2ed7dfa06864f7cc6b934c736367441482a89
chloehequet <chloe.hequet@gmail.com>
2023-05-15 13:22:33 +0100

    new lime baseline [2,0] - debug

--- perturb_and_transform.py
+++ perturb_and_transform.py
@@ -23,9 +23,11 @@ def from_interp_rep_transform(curr_sample: Tensor, original_input: Tensor, gene_
     for i in range(len(gene_index_array)):
         indices = gene_index_array[i]
         if curr_sample[0][i] == 0:
+            blocks = np.where(indices==0)[0]
             # set corresponding SNPs to 0 ALT alleles (2 REF)
-            perturbed_sample[indices,0] = 2
-            perturbed_sample[indices,1] = 0
+            # set first in each pair to 2 and 2nd to 0
+            perturbed_sample[blocks][::2] = 2
+            perturbed_sample[blocks][1::2] = 0
         else:
             pass
     return perturbed_sample.flatten()
