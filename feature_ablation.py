0a06b0b77756b4f8e933e1dc608de4ca947514d4
chloehequet <chloe.hequet@gmail.com>
2023-08-25 17:21:26 +0100

    lin null distrib for FTO

--- feature_ablation.py
+++ feature_ablation.py
@@ -5,8 +5,9 @@ import os
 import numpy as np
 from lime import get_test_set, get_masks
 
+use_cuda = torch.cuda.is_available()
+
 def single_gene_ablation(data, model, gene_keys, ordered_feature_masks, dict_file_name, lin_mod=False):
-    use_cuda = torch.cuda.is_available()
     device = torch.device("cuda:0" if (use_cuda and lin_mod==False) else "cpu")
     # data should be pre-filtered for BMI category and mse
     if not lin_mod:
@@ -42,55 +43,65 @@ def get_unsigned_means(diffs_dict, means_dict_path):
     pickle.dump(unsigned_means_dict, open(means_dict_path, "wb"))
     return unsigned_means_dict
 
-def pairwise_ablation(data, ordered_feature_masks, comparison_set, diffs_dict, model, dict_directory, lin_mod=False):
+def one_gene_pairwise(data, ordered_feature_masks, start_gene, gene_set,
+                      diffs_dict, model, dict_directory, lin_mod=False):
     # perturb given gene with comparison set and store perturbation results
     # comparison_set should be list of strings
     # data should be pre-filtered for BMI category and mse
-    use_cuda = torch.cuda.is_available()
+    pairs_dict = {}
+    if not lin_mod:
+        dict_path = dict_directory + start_gene + "_pairs_dict.pkl"
+    else:
+        dict_path = dict_directory + "LIN_" + start_gene + "_pairs_dict.pkl"
+    device = torch.device("cuda:0" if (use_cuda and lin_mod==False) else "cpu")
+    gene_mask = ordered_feature_masks[start_gene]
+    g = 1
+    for gene in gene_set:
+        print("gene %i of %i" % (g, len(gene_set)), end='\r')
+        key = start_gene + "_" + gene
+        mask = ordered_feature_masks[gene]
+        joint_mask = torch.tensor(gene_mask * mask).to(device)
+        diff_diffs = []
+        c = 0
+        for i in range(len(data)):
+            linear_diff = diffs_dict[start_gene][c] + diffs_dict[gene][c]
+            if lin_mod:
+                og_inp = data[i].reshape(1, -1)
+                og_pheno = model.predict(og_inp)
+                new_inp = (og_inp * joint_mask).reshape(1, -1)
+                new_pheno = model.predict(new_inp)
+                pair_diff = (og_pheno - new_pheno)
+            else:
+                og_inp = data[i].to(device)
+                og_pheno = model(og_inp.float())
+                new_inp = og_inp * joint_mask
+                new_pheno = model(new_inp.float())
+                pair_diff = (og_pheno - new_pheno).detach().cpu().numpy()
+            diff_diffs.append(pair_diff - linear_diff)
+            c += 1
+        pairs_dict[key] = np.mean(np.absolute(diff_diffs))
+        g += 1
+    print("writing pairs dict for %s to %s..." % (start_gene, dict_path))
+    pickle.dump(pairs_dict, open(dict_path, "wb"))
+
+#### NEED TO FIX THIS #####
+def pairwise_ablation(data, ordered_feature_masks, comparison_set,
+                      diffs_dict, model, dict_directory, lin_mod=False):
+    # exhaustive search of comparison set
     device = torch.device("cuda:0" if (use_cuda and lin_mod==False) else "cpu")
     searched_genes = set()
     for start_gene in comparison_set:
         print(start_gene)
-        pairs_dict = {}
-        dict_path = dict_directory + "LIN_" + start_gene + "_pairs_dict.pkl"
-        comparison_subset = [g for g in comparison_set if (g!=start_gene and g not in searched_genes)]
-        gene_mask = ordered_feature_masks[start_gene]
-        g = 1
-        for gene in comparison_subset:
-            print("gene %i of %i" % (g, len(comparison_subset)), end='\r')
-            key = start_gene + "_" + gene
-            mask = ordered_feature_masks[gene]
-            joint_mask = torch.tensor(gene_mask * mask).to(device)
-            diff_diffs = []
-            c = 0
-            for i in range(len(data)):
-                linear_diff = diffs_dict[start_gene][c] + diffs_dict[gene][c]
-                if lin_mod:
-                    og_inp = data[i].reshape(1, -1)
-                    og_pheno = model.predict(og_inp)
-                    new_inp = (og_inp * joint_mask).reshape(1, -1)
-                    new_pheno = model.predict(new_inp)
-                    pair_diff = (og_pheno - new_pheno)
-                else:
-                    og_inp = data[i].to(device)
-                    og_pheno = model(og_inp.float())
-                    new_inp = og_inp * joint_mask
-                    new_pheno = model(new_inp.float())
-                    pair_diff = (og_pheno - new_pheno).detach().cpu().numpy()
-                diff_diffs.append(pair_diff - linear_diff)
-                c += 1
-            pairs_dict[key] = np.mean(np.absolute(diff_diffs))
-            g += 1
-        print("writing pairs dict for %s to %s..."%(start_gene,dict_path))
-        pickle.dump(pairs_dict, open(dict_path, "wb"))
+        one_gene_pairwise(data, ordered_feature_masks, comparison_set, diffs_dict,
+                          model, dict_directory, lin_mod)
         searched_genes.add(start_gene)
     return True
 
 def main():
     # initialise
     ordered_feature_masks = pickle.load(open("../gene_masks/10k_full_genes_ordered_feature_masks.pkl", "rb"))
-    model = torch.load("NULL2_10000_4.pt")
-    # model = pickle.load(open("10000_enc_4_best_SGDRegressor.pkl","rb"))
+    # model = torch.load("NULL2_10000_4.pt")
+    model = pickle.load(open("10000_enc_4_best_SGDRegressor.pkl","rb"))
     print(model)
     test_samples = pickle.load(open("../sample_sets/testset.pkl", "rb"))
     pheno_dict = pickle.load(open("../phenotypes/scaled_phenotype_dict.pkl", "rb"))
@@ -114,19 +125,18 @@ def main():
     # print("beginning single gene ablation...")
     # lin_diffs = single_gene_ablation(X_data_filtered, model, gene_keys,
     #                                    ordered_feature_masks, "../diffs_dicts/linmod_diffs_dict.pkl", lin_mod=True)
-    # lin_diffs = pickle.load(open("../diffs_dicts/linmod_diffs_dict.pkl","rb"))
-    # lin_means = get_unsigned_means(lin_diffs, "../diffs_dicts/linmod_means_dict.pkl")
-    # null_means = pickle.load(open("../diffs_dicts/NULL2_means_dict.pkl","rb"))
-    # null_diffs = pickle.load(open("../diffs_dicts/NULL2_diffs_dict.pkl","rb"))
-    diffs_dict = pickle.load(open("../diffs_dicts/obese12diffs.pkl","rb"))
-    unsigned_means_dict = get_unsigned_means(diffs_dict, "../diffs_dicts/obese12means.pkl")
-    sorted_unsigned = sorted(unsigned_means_dict.items(), key=lambda x: x[1], reverse=True)
+    lin_diffs = pickle.load(open("../diffs_dicts/linmod_diffs_dict.pkl","rb"))
+    lin_means = get_unsigned_means(lin_diffs, "../diffs_dicts/linmod_means_dict.pkl")
+    # diffs_dict = pickle.load(open("../diffs_dicts/obese12diffs.pkl","rb"))
+    # unsigned_means_dict = get_unsigned_means(diffs_dict, "../diffs_dicts/obese12means.pkl")
+    sorted_unsigned = sorted(lin_means.items(), key=lambda x: x[1], reverse=True)
     # exhaustive search!
     print("beginning pairwise ablation...")
     # just do every tenth element - don't need to be exhaustive for null distrib
     # resume at RCAN
-    genes = [tup[0] for tup in sorted_unsigned[131:]]
-    pairwise_ablation(X_data_filtered, ordered_feature_masks, genes, diffs_dict, model, "../diffs_dicts/", lin_mod=False)
+    genes = [tup[0] for tup in sorted_unsigned[1:]]
+    one_gene_pairwise(X_data_filtered, ordered_feature_masks, "FTO", genes, lin_diffs, model, "../diffs_dicts/", lin_mod=True)
+    # pairwise_ablation(X_data_filtered, ordered_feature_masks, genes, lin_diffs, model, "../diffs_dicts/", lin_mod=False)
 
 if __name__ == "__main__":
     main()
