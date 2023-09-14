5a8202c139d269015bae258f037c978df34f9aad
chloehequet <chloe.hequet@gmail.com>
2023-09-14 14:31:17 +0100

    cleaned up earlier mess removed parallel cpu option reverted to previous method

--- feature_ablation.py
+++ feature_ablation.py
@@ -115,8 +115,9 @@ def one_gene_pairwise(data, ordered_feature_masks, start_gene, gene_set,
     pickle.dump(pairs_dict, open(dict_path, "wb"))
 
 def pairwise_ablation(data, ordered_feature_masks, gene_set,
-                      diffs_dict, stop_gene, searched_genes, model, dict_directory, device, lin_mod=False, parallel=False):
+                      diffs_dict, stop_gene, model, dict_directory, device, lin_mod=False, parallel=False):
     # exhaustive search of comparison set
+    searched_genes = set()
     for start_gene in gene_set:
         print(start_gene)
         if start_gene == stop_gene:
@@ -197,16 +198,10 @@ def main(start_index, stop_index, gpu, lin):
     sorted_unsigned = sorted(unsigned_means_dict.items(), key=lambda x:x[1], reverse=True)
     # exhaustive search!
     print("beginning pairwise ablation...")
-    genes = [tup[0] for tup in sorted_unsigned]
-    searched = set(pickle.load(open("searched_genes.pkl","rb")))
-    unsearched = [gene for gene in genes if gene not in searched][:20]
-    print("searched: ", len(searched))
-    print("remaining: ", len(unsearched))
-    print(unsearched)
-    for gene in unsearched:
-        comparison_set = genes[genes.index(gene):]
-        one_gene_pairwise(X_data_filtered, ordered_feature_masks, gene, comparison_set,
-                          diffs_dict, model, "../diffs_dicts/", device)
+    genes = [tup[0] for tup in sorted_unsigned[start_index:]]
+    stop_gene = sorted_unsigned[stop_index][0]
+    pairwise_ablation(X_data_filtered, ordered_feature_masks, genes, diffs_dict, stop_gene, model,
+                      "../diffs_dicts/", lin_mod=linmod, parallel=False)
 
 if __name__ == "__main__":
     main(start_index = int(sys.argv[1]), stop_index = int(sys.argv[2]), gpu = sys.argv[3], lin=int(sys.argv[4]))
